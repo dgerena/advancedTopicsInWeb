2029b75c4e940bb9703ded64960db491be732414
dgerena <dgerena@fullsail.edu>
2014-07-31 09:31:43 -0400

    test 7
    got a callback added in to take care of the async. due to it all
    happening at once the value was never there initially.

--- capstone/js/main.js
+++ capstone/js/main.js
@@ -1,23 +1,15 @@
 $(document).ready(function(){
+	// xively shit starts here.
 	xively.setKey( "7mwhzeLKvubmGwT49aJH7Aztn6tbGETGnHraIcrooYP2qkVc" );
 	xively.feed.get( "504", function( data ) {  
-	  console.log( data.title ); // Logs the feed title  
+	  console.log( data.title );// Logs the feed title  
 	});
 	xively.datastream.subscribe( "61916", "sine60", function( event, data ) {  
 	  console.log( data.current_value ); // Logs value changes in realtime  
-	});   
-	//////////////////////
-	// / Make sure the document is ready to be handled  
-	// $(document).ready(function($) {  
-
-	//   // Set the Xively API key (https://xively.com/users/YOUR_USERNAME/keys)  
-	//   xively.setKey( "PASTE_YOUR_KEY_HERE" );  
-
-	// Replace with your own values  
+	});    
 	var feedID        = '544049360';          // Feed ID  
 	  datastreamID  = "sensor_reading1";       // Datastream ID  
 	  selector      = "#test";   // Your element on the page  
-
 	// Get datastream data from Xively  
 	xively.datastream.get (feedID, datastreamID, function ( datastream ) {  
 		// WARNING: This code is only executed when we get a response back from Xively,   
@@ -29,7 +21,6 @@ $(document).ready(function(){
 
 		// Display the current value from the datastream  
 		$(selector).html( datastream["current_value"] );  
-
 		// Getting realtime!   
 		// The function associated with the subscribe method will be executed   
 		// every time there is an update to the datastream  
@@ -37,104 +28,107 @@ $(document).ready(function(){
 		  // Display the current value from the updated datastream  
 		  $(selector).html( datastream_updated["current_value"] );  
 		});  
-	});  
+	});
 	// WARNING: Code here will continue executing while we get the datastream data from Xively,   
 	// use the function associated with datastream.get to work with the data   
-	// once the request is complete  
-	var canvas = document.getElementById('myCanvas');	
-	if(canvas.getContext){
-		var ctx = canvas.getContext('2d');
-		// drawing code here
-		ctx.beginPath(); 
-		ctx.lineWidth="5";
-		ctx.strokeStyle="green";
-		ctx.moveTo(100,0);
-		ctx.lineTo(100,600);
-		ctx.stroke();
-
-		ctx.beginPath(); 
-		ctx.lineWidth="5";
-		ctx.strokeStyle="green";
-		ctx.moveTo(500,0);
-		ctx.lineTo(400,250);
-		ctx.stroke();
-
-		ctx.beginPath(); 
-		ctx.lineWidth="5";
-		ctx.strokeStyle="green";
-		ctx.moveTo(400,250);
-		ctx.lineTo(200,350);
-		ctx.stroke();
-
-		ctx.beginPath(); 
-		ctx.lineWidth="5";
-		ctx.strokeStyle="green";
-		ctx.moveTo(200,350);
-		ctx.lineTo(200,0);
-		ctx.stroke();
-
-		ctx.beginPath(); 
-		ctx.lineWidth="5";
-		ctx.strokeStyle="green";
-		ctx.moveTo(200,0);
-		ctx.lineTo(500,0);
-		ctx.stroke();
-
-		ctx.beginPath(); 
-		ctx.lineWidth="5";
-		ctx.strokeStyle="green";
-		ctx.moveTo(400,350);
-		ctx.lineTo(200,450);
-		ctx.stroke();
-
-		ctx.beginPath(); 
-		ctx.lineWidth="5";
-		ctx.strokeStyle="green";
-		ctx.moveTo(400,350);
-		ctx.lineTo(600,0);
-		ctx.stroke();
-
-		ctx.beginPath(); 
-		ctx.lineWidth="5";
-		ctx.strokeStyle="green";
-		ctx.moveTo(200,450);
-		ctx.lineTo(200,600);
-		ctx.stroke();
-
-		// arrow
-		$(function(){
-			console.log($("test").val());
-			if (document.getElementById("test").text=='1.00'){
-				ctx.beginPath(); 
-				ctx.lineWidth="5";
-				ctx.strokeStyle="green";
-				ctx.moveTo(150,400);
-				ctx.lineTo(150,200);
-				ctx.stroke();	
-				ctx.beginPath(); 
-				ctx.lineWidth="5";
-				ctx.strokeStyle="red";
-				ctx.moveTo(200,400);
-				ctx.lineTo(380,310);
-				ctx.stroke();	
-			}else{
-				ctx.beginPath(); 
-				ctx.lineWidth="5";
-				ctx.strokeStyle="red";
-				ctx.moveTo(150,400);
-				ctx.lineTo(150,200);
-				ctx.stroke();	
-				ctx.beginPath(); 
-				ctx.lineWidth="5";
-				ctx.strokeStyle="green";
-				ctx.moveTo(200,400);
-				ctx.lineTo(380,310);
-				ctx.stroke();	
-			}
-		});
-	};
+	// once the request is complete
+	var callback= $.Callbacks();
+	callbacks.add(draw());
 });
+function draw(){
+		var canvas = document.getElementById('myCanvas');	
+		if(canvas.getContext){
+			var ctx = canvas.getContext('2d');
+			// drawing code here
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(100,0);
+			ctx.lineTo(100,600);
+			ctx.stroke();
 
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(500,0);
+			ctx.lineTo(400,250);
+			ctx.stroke();
+
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(400,250);
+			ctx.lineTo(200,350);
+			ctx.stroke();
+
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(200,350);
+			ctx.lineTo(200,0);
+			ctx.stroke();
+
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(200,0);
+			ctx.lineTo(500,0);
+			ctx.stroke();
+
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(400,350);
+			ctx.lineTo(200,450);
+			ctx.stroke();
+
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(400,350);
+			ctx.lineTo(600,0);
+			ctx.stroke();
+
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(200,450);
+			ctx.lineTo(200,600);
+			ctx.stroke();
+
+			// arrow
+			$(function(){
+				console.log($("#test").val());
+				if (document.getElementById("test").text=='1.00'){
+					ctx.beginPath(); 
+					ctx.lineWidth="5";
+					ctx.strokeStyle="green";
+					ctx.moveTo(150,400);
+					ctx.lineTo(150,200);
+					ctx.stroke();	
+					ctx.beginPath(); 
+					ctx.lineWidth="5";
+					ctx.strokeStyle="red";
+					ctx.moveTo(200,400);
+					ctx.lineTo(380,310);
+					ctx.stroke();	
+				}else{
+					ctx.beginPath(); 
+					ctx.lineWidth="5";
+					ctx.strokeStyle="red";
+					ctx.moveTo(150,400);
+					ctx.lineTo(150,200);
+					ctx.stroke();	
+					ctx.beginPath(); 
+					ctx.lineWidth="5";
+					ctx.strokeStyle="green";
+					ctx.moveTo(200,400);
+					ctx.lineTo(380,310);
+					ctx.stroke();	
+				}
+			});
+		};
+	};
 /////////////////////////
 	
 	//http://dgerena.github.io/advancedTopicsInWeb/capstone/index.html github link.
