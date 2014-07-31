fb485b4bdc6718a605d4c3f1e1461ee5cf3dea91
dgerena <dgerena@fullsail.edu>
2014-07-31 02:24:51 -0400

    test2

--- capstone/js/main.js
+++ capstone/js/main.js
@@ -43,110 +43,111 @@ $(document).ready(function(){
   // WARNING: Code here will continue executing while we get the datastream data from Xively,   
   // use the function associated with datastream.get to work with the data   
   // once the request is complete  
+  var canvas = document.getElementById('myCanvas');
+	if (canvas.getContext){
+		var ctx = canvas.getContext('2d');
+		// drawing code here
+		ctx.beginPath(); 
+		ctx.lineWidth="5";
+		ctx.strokeStyle="green";
+		ctx.moveTo(100,0);
+		ctx.lineTo(100,600);
+		ctx.stroke();
+		
+		ctx.beginPath(); 
+		ctx.lineWidth="5";
+		ctx.strokeStyle="green";
+		ctx.moveTo(500,0);
+		ctx.lineTo(400,250);
+		ctx.stroke();
+
+		ctx.beginPath(); 
+		ctx.lineWidth="5";
+		ctx.strokeStyle="green";
+		ctx.moveTo(400,250);
+		ctx.lineTo(200,350);
+		ctx.stroke();
+
+		ctx.beginPath(); 
+		ctx.lineWidth="5";
+		ctx.strokeStyle="green";
+		ctx.moveTo(200,350);
+		ctx.lineTo(200,0);
+		ctx.stroke();
+
+		ctx.beginPath(); 
+		ctx.lineWidth="5";
+		ctx.strokeStyle="green";
+		ctx.moveTo(200,0);
+		ctx.lineTo(500,0);
+		ctx.stroke();
+
+		ctx.beginPath(); 
+		ctx.lineWidth="5";
+		ctx.strokeStyle="green";
+		ctx.moveTo(400,350);
+		ctx.lineTo(200,450);
+		ctx.stroke();
+
+		ctx.beginPath(); 
+		ctx.lineWidth="5";
+		ctx.strokeStyle="green";
+		ctx.moveTo(400,350);
+		ctx.lineTo(600,0);
+		ctx.stroke();
+
+		ctx.beginPath(); 
+		ctx.lineWidth="5";
+		ctx.strokeStyle="green";
+		ctx.moveTo(200,450);
+		ctx.lineTo(200,600);
+		ctx.stroke();
+
+		// arrow
+		
+		if (document.getElementById("test").value==1.00) {
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(150,400);
+			ctx.lineTo(150,200);
+			ctx.stroke();	
+			
+			
+
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="red";
+			ctx.moveTo(200,400);
+			ctx.lineTo(380,310);
+			ctx.stroke();	
+			//if statement for the direction in the header.
+						
+		}else{
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="red";
+			ctx.moveTo(150,400);
+			ctx.lineTo(150,200);
+			ctx.stroke();	
+			//if statement for the direction in the header.
+			
+
+			ctx.beginPath(); 
+			ctx.lineWidth="5";
+			ctx.strokeStyle="green";
+			ctx.moveTo(200,400);
+			ctx.lineTo(380,310);
+			ctx.stroke();	
+			//if statement for the direction in the header.
+			
+		}
+	} else {
+	  // canvas-unsupported code here
+	}
 });  
 
 /////////////////////////
-// 	var canvas = document.getElementById('myCanvas');
-// 	if (canvas.getContext){
-// 		var ctx = canvas.getContext('2d');
-// 		// drawing code here
-// 		ctx.beginPath(); 
-// 		ctx.lineWidth="5";
-// 		ctx.strokeStyle="green";
-// 		ctx.moveTo(100,0);
-// 		ctx.lineTo(100,600);
-// 		ctx.stroke();
-		
-// 		ctx.beginPath(); 
-// 		ctx.lineWidth="5";
-// 		ctx.strokeStyle="green";
-// 		ctx.moveTo(500,0);
-// 		ctx.lineTo(400,250);
-// 		ctx.stroke();
-
-// 		ctx.beginPath(); 
-// 		ctx.lineWidth="5";
-// 		ctx.strokeStyle="green";
-// 		ctx.moveTo(400,250);
-// 		ctx.lineTo(200,350);
-// 		ctx.stroke();
-
-// 		ctx.beginPath(); 
-// 		ctx.lineWidth="5";
-// 		ctx.strokeStyle="green";
-// 		ctx.moveTo(200,350);
-// 		ctx.lineTo(200,0);
-// 		ctx.stroke();
-
-// 		ctx.beginPath(); 
-// 		ctx.lineWidth="5";
-// 		ctx.strokeStyle="green";
-// 		ctx.moveTo(200,0);
-// 		ctx.lineTo(500,0);
-// 		ctx.stroke();
-
-// 		ctx.beginPath(); 
-// 		ctx.lineWidth="5";
-// 		ctx.strokeStyle="green";
-// 		ctx.moveTo(400,350);
-// 		ctx.lineTo(200,450);
-// 		ctx.stroke();
-
-// 		ctx.beginPath(); 
-// 		ctx.lineWidth="5";
-// 		ctx.strokeStyle="green";
-// 		ctx.moveTo(400,350);
-// 		ctx.lineTo(600,0);
-// 		ctx.stroke();
-
-// 		ctx.beginPath(); 
-// 		ctx.lineWidth="5";
-// 		ctx.strokeStyle="green";
-// 		ctx.moveTo(200,450);
-// 		ctx.lineTo(200,600);
-// 		ctx.stroke();
-
-// 		// arrow
-		
-// 		if (false) {
-// 			ctx.beginPath(); 
-// 			ctx.lineWidth="5";
-// 			ctx.strokeStyle="green";
-// 			ctx.moveTo(150,400);
-// 			ctx.lineTo(150,200);
-// 			ctx.stroke();	
-			
-			
-
-// 			ctx.beginPath(); 
-// 			ctx.lineWidth="5";
-// 			ctx.strokeStyle="red";
-// 			ctx.moveTo(200,400);
-// 			ctx.lineTo(380,310);
-// 			ctx.stroke();	
-// 			//if statement for the direction in the header.
-						
-// 		}else{
-// 			ctx.beginPath(); 
-// 			ctx.lineWidth="5";
-// 			ctx.strokeStyle="red";
-// 			ctx.moveTo(150,400);
-// 			ctx.lineTo(150,200);
-// 			ctx.stroke();	
-// 			//if statement for the direction in the header.
-			
-
-// 			ctx.beginPath(); 
-// 			ctx.lineWidth="5";
-// 			ctx.strokeStyle="green";
-// 			ctx.moveTo(200,400);
-// 			ctx.lineTo(380,310);
-// 			ctx.stroke();	
-// 			//if statement for the direction in the header.
-			
-// 		}
-// 	} else {
-// 	  // canvas-unsupported code here
-// 	}
-// 	//http://dgerena.github.io/advancedTopicsInWeb/capstone/index.html github link.
-// });
+	
+	//http://dgerena.github.io/advancedTopicsInWeb/capstone/index.html github link.
+});
